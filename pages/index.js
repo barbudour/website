893d424640b70d1a22bbac96c357878a26f6d329
Ivan Vinokurov <barbudour@gmail.com>
2021-08-22 22:53:58 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Index() {
         <source src="/bg.mp4" type="video/mp4" />
       </video>
       <Layout pageTitle="Poimandres">
-        <a href="https://github.com/pmndrs">
-          <h1>Poimandres</h1>
+        <a href="https://github.com/barbudour">
+          <h1>barbudour / senior frontend developer</h1>
         </a>
       </Layout>
     </>
